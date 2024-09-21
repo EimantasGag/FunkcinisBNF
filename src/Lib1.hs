686aa3408f41992606c5f3ee6b4a83d48e11bf55
EimantasGag <eimis.gagilas@gmail.com>
2024-09-21 21:51:55 +0300

    Nauji auto completions

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,4 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = ["slotid","slotgame","betamount","gamble"]
+completions = ["Order","Add","Cancel","Cola","Water","Juice","Wine","Beer","Fries","Garlic Bread","Salad","Soup","Pizza","Burger","Pasta","Salad","Steak","Sushi"]
